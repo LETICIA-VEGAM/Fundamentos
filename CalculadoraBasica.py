56fbdd18347daeb0706b6c053a1dff849120a4e5
LETICIA-VEGAM <leticiavegam19@gmail.com>
2025-07-01 09:04:11 -0600

    Add files via upload

---
+++ CalculadoraBasica.py
@@ -0,0 +1,8 @@
+from Principal import nuevoTema
+import Calc.operaciones as c
+
+nuevoTema("Módulos")
+print(c.suma(1, 2, 2, 4))
+print(c.resta(40, 30, 20, 10))
+print(c.mult(3, 4, 5))
+print(c.div(25, 3))
